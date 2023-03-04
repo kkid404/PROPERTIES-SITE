ec574e36c7e42e5a737b359d7b73f45a35313c44
kkid404 <vizzy1488@gmail.com>
2023-03-04 14:38:02 +0300

    Исзменил время кеширования

--- index.php
+++ index.php
@@ -39,70 +39,99 @@ $APPLICATION->SetTitle("Биржа недвижимости");
 <div class="site-section site-section-sm bg-light">
 	<div class="container">
 		 <?$APPLICATION->IncludeComponent(
-	"bitrix:news.line",
-	"tiles",
-	Array(
+	"bitrix:news.line", 
+	"tiles", 
+	array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "300",
+		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
 		"COMPONENT_TEMPLATE" => "tiles",
 		"DETAIL_URL" => "",
-		"FIELD_CODE" => array(0=>"PREVIEW_TEXT",1=>"PREVIEW_PICTURE",2=>"PROPERTY_PRICE",3=>"PROPERTY_TOTAL_AREA",4=>"PROPERTY_COUNT_FLOORS",5=>"PROPERTY_COUNT_BATHROOMS",),
-		"IBLOCKS" => array(0=>"5",),
+		"FIELD_CODE" => array(
+			0 => "PREVIEW_TEXT",
+			1 => "PREVIEW_PICTURE",
+			2 => "PROPERTY_PRICE",
+			3 => "PROPERTY_TOTAL_AREA",
+			4 => "PROPERTY_COUNT_FLOORS",
+			5 => "PROPERTY_COUNT_BATHROOMS",
+			6 => "",
+		),
+		"IBLOCKS" => array(
+			0 => "5",
+		),
 		"IBLOCK_TYPE" => "posts",
 		"NEWS_COUNT" => "9",
 		"SORT_BY1" => "ACTIVE_FROM",
 		"SORT_BY2" => "SORT",
 		"SORT_ORDER1" => "DESC",
 		"SORT_ORDER2" => "ASC"
-	)
+	),
+	false
 );?>
 	</div>
 </div>
 <div class="site-section">
 	<div class="container">
 		 <?$APPLICATION->IncludeComponent(
-	"bitrix:news.line",
-	"tiles_services",
-	Array(
+	"bitrix:news.line", 
+	"tiles_services", 
+	array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "300",
+		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
 		"DETAIL_URL" => "",
-		"FIELD_CODE" => array(0=>"PREVIEW_TEXT",1=>"PREVIEW_PICTURE",2=>"PROPERTY_EXTERNAL_LINK",),
-		"IBLOCKS" => array(0=>"6",),
+		"FIELD_CODE" => array(
+			0 => "PREVIEW_TEXT",
+			1 => "PREVIEW_PICTURE",
+			2 => "PROPERTY_EXTERNAL_LINK",
+			3 => "",
+		),
+		"IBLOCKS" => array(
+			0 => "6",
+		),
 		"IBLOCK_TYPE" => "services",
 		"NEWS_COUNT" => "20",
 		"SORT_BY1" => "ACTIVE_FROM",
 		"SORT_BY2" => "SORT",
 		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC"
-	)
+		"SORT_ORDER2" => "ASC",
+		"COMPONENT_TEMPLATE" => "tiles_services"
+	),
+	false
 );?>
 	</div>
 </div>
 <div class="site-section bg-light">
 	<div class="container">
 		 <?$APPLICATION->IncludeComponent(
-	"bitrix:news.line",
-	"our_blog",
-	Array(
+	"bitrix:news.line", 
+	"our_blog", 
+	array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "300",
+		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
 		"DETAIL_URL" => "",
-		"FIELD_CODE" => array(0=>"NAME",1=>"PREVIEW_TEXT",2=>"PREVIEW_PICTURE",3=>"",),
-		"IBLOCKS" => array(0=>"1",),
+		"FIELD_CODE" => array(
+			0 => "NAME",
+			1 => "PREVIEW_TEXT",
+			2 => "PREVIEW_PICTURE",
+			3 => "",
+		),
+		"IBLOCKS" => array(
+			0 => "1",
+		),
 		"IBLOCK_TYPE" => "news",
 		"NEWS_COUNT" => "3",
 		"SORT_BY1" => "ACTIVE_FROM",
 		"SORT_BY2" => "SORT",
 		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC"
-	)
+		"SORT_ORDER2" => "ASC",
+		"COMPONENT_TEMPLATE" => "our_blog"
+	),
+	false
 );?>
 	</div>
 </div>
