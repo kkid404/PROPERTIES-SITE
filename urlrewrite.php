3a23f7d5d902f052d05587efa423e12b5b3f0c9b
kkid404 <vizzy1488@gmail.com>
2023-03-05 12:13:19 +0300

    Task 6

--- urlrewrite.php
+++ urlrewrite.php
@@ -8,7 +8,7 @@ $arUrlRewrite=array (
     'PATH' => '/lichnyy-kabinet-prodavtsa/moi-obyavleniya-/index.php',
     'SORT' => 100,
   ),
-  6 => 
+  13 => 
   array (
     'CONDITION' => '#^/o-servise/vakansii/#',
     'RULE' => '',
